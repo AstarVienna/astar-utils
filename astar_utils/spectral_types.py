766fdc40088a683eb45dcd5ac588fccbea131ee2
teutoburg <ghost@instruct.at>
2024-09-10 14:00:24 +0200

    Fix regex, proper types, remove xfail for xpassing tests

--- astar_utils/spectral_types.py
+++ astar_utils/spectral_types.py
@@ -5,16 +5,16 @@ import re
 from dataclasses import dataclass, field
 
 regex = re.compile(r"^(?P<spec_cls>[OBAFGKM])(?P<sub_cls>\d(?:\.\d)?)?"
-                   "(?P<lum_cls> I{0,3}|IV|V)?$", re.A | re.I)
+                   "(?P<lum_cls>I{1,3}|IV|V)?$", re.A | re.I)
 
 
 @dataclass
 class SpectralType:
     """TBA."""
 
-    spectral_class: str = field(init=False, repr=False, default=None)
-    spectral_subclass: str = field(init=False, repr=False, default=None)
-    luminosity_class: str = field(init=False, repr=False, default=None)
+    spectral_class: str | None = field(init=False, repr=False, default=None)
+    spectral_subclass: float | None = field(init=False, repr=False, default=None)
+    luminosity_class: str | None = field(init=False, repr=False, default=None)
     spectype: str
 
     def __post_init__(self) -> None:
@@ -24,5 +24,7 @@ class SpectralType:
 
         classes = match.groupdict()
         self.spectral_class = classes["spec_cls"]
-        self.spectral_subclass = classes["sub_cls"]
         self.luminosity_class = classes["lum_cls"]
+
+        if classes["sub_cls"] is not None:
+            self.spectral_subclass = float(classes["sub_cls"])
