0abf4590dadca16181bbce6df5107e0e603e4fa8
teutoburg <ghost@instruct.at>
2024-09-10 13:02:36 +0200

    Add initial files and imports

---
+++ astar_utils/spectral_types.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""."""
+
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class SpectralType:
+    """TBA."""
+
+    pass
---
+++ tests/test_spectral_types.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Unit tests for spectral_types.py."""
+
+import pytest
+
+from astar_utils import SpectralType
