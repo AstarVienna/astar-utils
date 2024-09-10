4c0065677ae84acaf602e04c095266668d9a99f4
teutoburg <ghost@instruct.at>
2024-09-10 13:39:36 +0200

    Add initial implementation for SpectralType

--- astar_utils/spectral_types.py
+++ astar_utils/spectral_types.py
@@ -2,11 +2,27 @@
 """."""
 
 import re
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+regex = re.compile(r"^(?P<spec_cls>[OBAFGKM])(?P<sub_cls>\d(?:\.\d)?)?"
+                   "(?P<lum_cls> I{0,3}|IV|V)?$", re.A | re.I)
 
 
 @dataclass
 class SpectralType:
     """TBA."""
 
-    pass
+    spectral_class: str = field(init=False, repr=False, default=None)
+    spectral_subclass: str = field(init=False, repr=False, default=None)
+    luminosity_class: str = field(init=False, repr=False, default=None)
+    spectype: str
+
+    def __post_init__(self) -> None:
+        """Validate input and populate fields."""
+        if not (match := regex.fullmatch(self.spectype)):
+            raise ValueError(self.spectype)
+
+        classes = match.groupdict()
+        self.spectral_class = classes["spec_cls"]
+        self.spectral_subclass = classes["sub_cls"]
+        self.luminosity_class = classes["lum_cls"]
